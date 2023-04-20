97dbe9b8e240fc455d25c4ec779c988766566c7a
duca43 <dusan.maksic96@gmail.com>
2023-04-21 00:30:14 +0200

    Proposed changes from our meeting

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
-const titleShareButtons = document.querySelectorAll(".tile-share-btn")
-console.log(titleShareButtons)
+const titleShareButtons = document.querySelectorAll(".tile-share-btn");
+console.log(titleShareButtons);
 
-async function copyText(e){
-    // prevent from going to the website
-    e.preventDefault()
-    const link =  this.getAttribute("link")
-    console.log(link)
-    try{
-       await navigator.clipboard.writeText(link)
-       alert("You copied! -> " + link)
-    }catch(err){
-        console.error(err)
-    }
+const copyText = async (e) => {
+  // prevent from going to the website
+  e.preventDefault();
+  const link = e.currentTarget.getAttribute("link");
+  console.log(link);
+  try {
+    await navigator.clipboard.writeText(link);
+    alert("You copied! -> " + link);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-}
-
-titleShareButtons.forEach(shareButton => 
-    shareButton.addEventListener("click", copyText))
-
+titleShareButtons.forEach((shareButton) =>
+  shareButton.addEventListener("click", copyText)
+);
 
 // // GSAP animations :)
 // removing for now
 
-const topShareButton = document.querySelector(".share-button")
-const bars = document.querySelectorAll(".bar")
-gsap.from(topShareButton, {duration: 3,x: -600, y: () => Math.random() * 400 - 200, rotate: 4320})
-gsap.from(bars, {duration: 2, x:-500, y:200, rotate: 1080, stagger: 0.25})
-
+const topShareButton = document.querySelector(".share-button");
+const bars = document.querySelectorAll(".bar");
+gsap.from(topShareButton, {
+  duration: 3,
+  x: -600,
+  y: () => Math.random() * 400 - 200,
+  rotate: 4320,
+});
+gsap.from(bars, { duration: 2, x: -500, y: 200, rotate: 1080, stagger: 0.25 });
 
 // event for the hamburger menu
 
-const hamburgerMenu = document.querySelector(".hamburger-menu")
+const hamburgerMenu = document.querySelector(".hamburger-menu");
 
-function hamburgerEvent() {
-    gsap.from(bars, {duration: 1.7, opacity: 0, x:() => Math.random() * 200 - 400, y:() => Math.random() * 400 - 200, rotate: 1080, stagger: 0.25})
-}
+// Indicator if hamburger animation is being run or not
+let hamburgerAnimationRunning = false;
+
+const hamburgerEvent = () => {
+  console.log(hamburgerAnimationRunning);
+  if (hamburgerAnimationRunning) {
+    return;
+  }
+
+  // Putting the hamburger animation indicator on true before running the actual animation
+  hamburgerAnimationRunning = true;
+  console.log(hamburgerAnimationRunning);
+  gsap.from(bars, {
+    duration: 1.7,
+    opacity: 0,
+    x: () => Math.random() * 200 - 400,
+    y: () => Math.random() * 400 - 200,
+    rotate: 1080,
+    stagger: 0.25,
+    onComplete: () => (hamburgerAnimationRunning = false),
+  });
+};
